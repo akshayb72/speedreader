8b200f15c92445c3af3d45bd06857ed52462e0d6
Akshay B <77104366+akshayb72@users.noreply.github.com>
2024-03-16 12:32:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 console.log("Happy Testing");
 console.log("Happy Hunting");
 let mode = false;
-const textContainer = document.getElementById('text');
-
+const text = document.getElementById('text');
+let time = 60000;
+let wpm = 120;
 function toggleDarkmode() {
     const body = document.getElementById('bodyMode');
     const toggleButton = document.getElementById('toggleMode');
@@ -21,23 +22,29 @@ function toggleDarkmode() {
     }
 }
 
-function readPrompt()
-{
-    let text = ["hi","bye"];
 
-    text.forEach((word,index) => {
-        const span = document.createElement('span');
-        span.textContent = word + ' ';
+function readPrompt(wpm) {
+    let text = ["hi", "try","bye"];
+    const paragraph = document.getElementById("text"); 
 
-        
-    // Assign different colors based on index
-    if (index % 2 === 0) {
-        span.style.color = 'red';
-    } else {
-        span.style.color = 'red';
+    let index = 0; 
+
+    function writeWord() {
+        if (index < text.length) {
+            const word = text[index];
+            const span = document.createElement("span");
+            span.textContent = word;
+
+            paragraph.textContent = '';
+
+            paragraph.appendChild(span);
+
+            index++; 
+
+            setTimeout(writeWord, 60000/120); 
+        }
     }
-    console.log(textContainer,span);
-        textContainer.textContent(span);
 
-    });
+    writeWord();
 }
+
