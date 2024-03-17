bc79980aa1612d0936ed1aa420b2ff92ff7da70f
Akshay B <77104366+akshayb72@users.noreply.github.com>
2024-03-17 20:46:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,32 @@ console.log("Happy Testing");
 console.log("Happy Hunting");
 let mode = false;
 const text = document.getElementById('text');
+
 let time = 60000;
 let wpm = 120;
 function toggleDarkmode() {
-    const body = document.getElementById('bodyMode');
-    const toggleButton = document.getElementById('toggleMode');
+    var toggleButton = document.getElementById('toggleMode');
 
+    const body = document.getElementById('bodyMode');
     if (mode ===false)
     {
         body.style.backgroundColor = "#000000"
         body.style.color = "#ffffff"
         mode = true;
+        lightMode();
     }
     else if(mode === true)
     {
         body.style.backgroundColor = "#ffffff"
         body.style.color = "#000000"
+        
         mode = false;
+        darkMode();
     }
 }
 
-
 function readPrompt(wpm) {
-    let text = ["hi", "try","bye"];
+    let text = [""];
     const paragraph = document.getElementById("text"); 
 
     let index = 0; 
@@ -48,3 +51,53 @@ function readPrompt(wpm) {
     writeWord();
 }
 
+function darkMode()
+{
+    const toggle = document.getElementById('toggleMode');
+
+    toggle.innerHTML = '';
+    moon=`        <svg width="30" height="30" style="">
+    <path
+      fill="currentColor"
+      d="
+        M 23, 5
+        A 12 12 0 1 0 23, 25
+        A 12 12 0 0 1 23, 5"
+    />
+  </svg>
+ `;
+    toggle.insertAdjacentHTML('beforeend',moon);
+
+}
+
+function lightMode()
+{
+const toggle = document.getElementById('toggleMode');
+
+toggle.innerHTML = '';
+const sun = `  <svg width="30" height="30" style ="color:black;">
+<circle cx="15" cy="15" r="6" fill="currentColor" />
+
+<line
+  id="ray"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  x1="15"
+  y1="1"
+  x2="15"
+  y2="4"
+></line>
+
+<use href="#ray" transform="rotate(45 15 15)" />
+<use href="#ray" transform="rotate(90 15 15)" />
+<use href="#ray" transform="rotate(135 15 15)" />
+<use href="#ray" transform="rotate(180 15 15)" />
+<use href="#ray" transform="rotate(225 15 15)" />
+<use href="#ray" transform="rotate(270 15 15)" />
+<use href="#ray" transform="rotate(315 15 15)" />
+</svg> `;
+
+toggle.insertAdjacentHTML('beforeend',sun);
+
+}
